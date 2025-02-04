f9326bfd4d8b2944413c1027ce8b6035a16d2e77
ArendZaag <89770214+ArendZaag@users.noreply.github.com>
2025-02-04 14:34:14 +0100

    Zoekfilter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,6 @@
     import FInfo from "$lib/components/footerInfo.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-500">
-    Visit <a href="about.svelte">svelte.dev/docs/kit</a> to read the documentation
-</p>
 
 <Products />
 <FInfo />
