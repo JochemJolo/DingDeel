b3e5b7af3bf4478be0855586b26b2a6a361e8bc8
Jochem <lootsmajochem@gmail.com>
2025-02-04 12:45:46 +0100

    we got foot

--- src/lib/components/footerInfo.svelte
+++ src/lib/components/footerInfo.svelte
@@ -1,14 +1,39 @@
 <script>
-	import logo from '$lib/assets/Cambuur.png';
+	import logo from '$lib/assets/GreyPlanks.png';
 </script>
 
 
-<style>
+<!-- <style>
     .full-width {
-      width: 100vw; /* 100% of the viewport width */
-      height: 200pc; /* Maintain aspect ratio */
-      display: block; /* Remove default inline spacing */
+      width: 100vw; 
+      height: 15cm;
+      display: block; 
+      background-image: 'logo';
     }
   </style>
 
-<img alt="The project logo" src={logo} class="full-width"/>
+  
+  <div class="full-width" style="background-image: url({logo});">
+
+<img alt="The project logo" src={logo} class="full-width"/> -->
+  
+  <div class="full-width" >
+    <div class="p-12 text-center rounded-lg">
+        Test
+    </div>
+  </div>
+  
+  <style>
+    .full-width {
+        background-color: rgb(190, 190, 190);
+        width: 100vw;
+        height: 5cm;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        opacity: 0.7;
+        background-size: 100% 100%;
+        background-position: bottom;
+    }
+</style>
+  
