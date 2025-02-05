d1e024b659ae2a8f264944d7d099255a65ffdfae
ZaneAhmed <zane.mahmoud212@gmail.com>
2025-02-05 14:29:40 +0100

    Product update

--- src/lib/components/Products.svelte
+++ src/lib/components/Products.svelte
@@ -1,7 +1,16 @@
 <script>
     import { productData } from "$lib/store/datastore.store";
+    import { onMount } from "svelte";
     import Zoekfilter from "./Zoekfilter.svelte";
-    let products = $productData;
+    $: products = shuffleArray($productData);
+
+    function shuffleArray(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); // Random index
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+        }
+        return arr;
+    }
 </script>
 
 <main class="p-4 flex">
@@ -11,16 +20,16 @@
     </aside>
 
     <section class="w-full max-w-7xl mx-auto px-4">
-        <h1 class="text-3xl font-bold mb-8 text-center">Artikelen</h1>
+        <h1 class="text-2xl font-bold mb-8 text-center">Beschikbare Items</h1>
         <div class="flex flex-wrap justify-center gap-8">
             {#each products as product}
                 <div
-                    class="flex-shrink-0 w-64 pb-4 bg-white rounded-xl shadow-md overflow-hidden transition-transform hover:scale-105 relative"
+                    class="flex-shrink-0 w-64 pb-4 bg-white rounded-xl shadow-md overflow-hidden transition-transform relative"
                 >
                     <img
                         src={product.image}
                         alt={product.name}
-                        class="w-full h-64 object-cover"
+                        class="w-full h-64 hover:scale-110 transition-transform object-cover"
                     />
                     <div
                         class="absolute top-6 -right-8 transform rotate-45
@@ -39,30 +48,50 @@
                         <h2 class="text-xl font-semibold mb-2 truncate">
                             {product.name}
                         </h2>
+                        {product.description}
                         <p class="text-sm text-gray-600">
                             Category: {product.productType}
                         </p>
                     </div>
-                    <button
-                        class="text-white font-semibold w-1/2 py-2 rounded-md
-    {product.type === 'Kopen'
-                            ? 'bg-red-500 hover:bg-red-600'
-                            : product.type === 'Ruilen'
-                              ? 'bg-blue-500 hover:bg-blue-600'
-                              : product.type === 'Lenen'
-                                ? 'bg-green-500 hover:bg-green-600'
-                                : 'bg-gray-500 hover:bg-gray-600'}"
+                    <div
+                        class="flex flex-col space-y-2 items-center justify-center"
                     >
                         {#if product.type === "Kopen"}
-                            Bieden
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-green-500 hover:bg-green-600"
+                            >
+                                Stuur Bericht
+                            </button>
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-red-500 hover:bg-red-600"
+                            >
+                                Bieden
+                            </button>
                         {:else if product.type === "Ruilen"}
-                            Ruil Voorstel
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-green-500 hover:bg-green-600"
+                            >
+                                Stuur Bericht
+                            </button>
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-blue-500 hover:bg-blue-600"
+                            >
+                                Ruil Voorstel
+                            </button>
                         {:else if product.type === "Lenen"}
-                            Stuur Bericht
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-green-500 hover:bg-green-600"
+                            >
+                                Stuur Bericht
+                            </button>
                         {:else}
-                            Stuur Bericht
+                            <button
+                                class="text-white font-semibold w-1/2 py-2 rounded-md bg-gray-500 hover:bg-gray-600"
+                            >
+                                Stuur Bericht
+                            </button>
                         {/if}
-                    </button>
+                    </div>
                 </div>
             {/each}
         </div>
--- src/routes/voertuigen/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Navbar from "$lib/components/Navbar.svelte";
-    import Products from "$lib/components/Products.svelte";
-    import FInfo from "$lib/components/footerInfo.svelte";
-</script>
-
-<Navbar />
-<Products />
-<FInfo />
