9a8338fb6505d4faed4e49dd786f1ce9c3954d52
Jochem <lootsmajochem@gmail.com>
2025-02-05 15:13:26 +0100

    Better Foot

--- src/lib/components/footerInfo.svelte
+++ src/lib/components/footerInfo.svelte
@@ -1,17 +1,34 @@
-<footer class="bg-gray-800 text-white py-8">
-    <div class="container mx-auto text-center">
-        <p class="hover:text-gray-400">&copy;Ding Deel.</p>
-        <div class="mt-4">
-            <a href="https://www.google.com/maps/place/Miau+Cafe/@44.4216604,26.107648,17.83z/data=!4m6!3m5!1s0x40b1ff217530320f:0x4335418785793418!8m2!3d44.421369!4d26.109326!16s%2Fg%2F11bxc6cb7c?entry=ttu&g_ep=EgoyMDI1MDIwMi4wIKXMDSoASAFQAw%3D%3D" class="text-white hover:text-gray-400 mx-2">Strada Între Gârle 7, București 040294, Romania</a>
-            <a href="/" class="text-white hover:text-gray-400 mx-2">Top</a>
-            <a href="/" class="text-white hover:text-gray-400 mx-2">About</a>
-            <a href="/" class="text-white hover:text-gray-400 mx-2">Services</a>
-            <a href="/" class="text-white hover:text-gray-400 mx-2">Contact</a>
-            <a href="/" class="text-white text-white hover:text-gray-400 mx-2">0515-8008136</a>
-            <a href="/" class="email">DingDeel@gmail.com</a>
+<footer class="bg-gray-900 text-gray-300 py-12">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+            <div class="text-center md:text-left">
+                <h3 class="text-xl font-semibold mb-4">Ding Deel</h3>
+                <p>&copy; 2025 Ding Deel. All rights reserved.</p>
+            </div>
+            <div class="text-center  md:text-left">
+                <h4 class="text-lg font-medium mb-4">Quick Links</h4>
+                <ul>
+                    <li><a href="/" class="hover:text-white transition duration-300">Top</a></li>
+                    <li><a href="/" class="hover:text-white transition duration-300">About</a></li>
+                    <li><a href="/" class="hover:text-white transition duration-300">Services</a></li>
+                    <li><a href="/" class="hover:text-white transition duration-300">Contact</a></li>
+                </ul>
+            </div>
+            <div class="text-center md:text-left">
+                <h4 class="text-lg font-medium mb-4">Location</h4>
+                <a href="https://www.google.com/maps/place/Miau+Cafe/@44.4216604,26.107648,17.83z/data=!4m6!3m5!1s0x40b1ff217530320f:0x4335418785793418!8m2!3d44.421369!4d26.109326!16s%2Fg%2F11bxc6cb7c?entry=ttu&g_ep=EgoyMDI1MDIwMi4wIKXMDSoASAFQAw%3D%3D" class="hover:text-white transition duration-300">
+                    Strada Între Gârle 7<br>
+                    București 040294<br>
+                    Romania
+                </a>
+            </div>
+            <div class="text-center md:text-left">
+                <h4 class="text-lg font-medium mb-4">Contact</h4>
+                <p><a href="tel:05158008136" class="hover:text-white transition duration-300">0515-8008136</a></p>
+                <p><a href="mailto:DingDeel@gmail.com" class="hover:text-white transition duration-300">DingDeel@gmail.com</a></p>
+            </div>
         </div>
     </div>
-
 </footer>
 
 <style>
