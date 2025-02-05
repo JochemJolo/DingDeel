0a76a4ef0040acf90dab39d4eed02cd2bbef17a4
Jochem <lootsmajochem@gmail.com>
2025-02-05 14:49:55 +0100

    Adress

--- src/lib/components/footerInfo.svelte
+++ src/lib/components/footerInfo.svelte
@@ -1,20 +1,24 @@
-<script>
-    import logo from "$lib/assets/Logo.png";
-</script>
-
 <footer class="bg-gray-800 text-white py-8">
     <div class="container mx-auto text-center">
-        <p>&copy;Ding Deel. All rights reserved.</p>
+        <p class="hover:text-gray-400">&copy;Ding Deel.</p>
         <div class="mt-4">
+            <a href="https://www.google.com/maps/place/Miau+Cafe/@44.4216604,26.107648,17.83z/data=!4m6!3m5!1s0x40b1ff217530320f:0x4335418785793418!8m2!3d44.421369!4d26.109326!16s%2Fg%2F11bxc6cb7c?entry=ttu&g_ep=EgoyMDI1MDIwMi4wIKXMDSoASAFQAw%3D%3D" class="text-white hover:text-gray-400 mx-2">Strada Între Gârle 7, București 040294, Romania</a>
             <a href="/" class="text-white hover:text-gray-400 mx-2">Top</a>
             <a href="/" class="text-white hover:text-gray-400 mx-2">About</a>
             <a href="/" class="text-white hover:text-gray-400 mx-2">Services</a>
             <a href="/" class="text-white hover:text-gray-400 mx-2">Contact</a>
+            <a href="/" class="text-white text-white hover:text-gray-400 mx-2">0515-8008136</a>
+            <a href="/" class="email">DingDeel@gmail.com</a>
         </div>
     </div>
 
 </footer>
 
-
-
-<!-- <img alt="The project logo" src={logo} /> -->
+<style>
+    .email {
+        color: lightblue;
+    }
+    .email:hover{
+        color: blue;
+    }
+</style>
